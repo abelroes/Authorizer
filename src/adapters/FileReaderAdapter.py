2a70199d2e3f2fcbaf34194072ac53523e979fc0
Abel Rocha <abel.roes@gmail.com>
2021-10-03 14:25:48 -0300

    Adjust file reader

--- src/adapters/FileReaderAdapter.py
+++ src/adapters/FileReaderAdapter.py
@@ -3,8 +3,5 @@ from typing import TextIO
 from controllers.JSONFormater import convert_to_json_dict
 
 def read_file(input_file: TextIO) -> list:
-    file_in_lines_list = []
     converter = convert_to_json_dict
-    for line in input_file:
-        file_in_lines_list.append(converter(line))
-    return file_in_lines_list
+    return [converter(line) for line in input_file]
--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 import sys
 from adapters.FileReaderAdapter import read_file
+from controllers.handlers.AuthorizerHandler import handle_input
 
 
 def main():
     input_data = read_file(sys.stdin)
-    print(input_data)
-    
-    
-
+    handle_input(input_data)
 
 if __name__ == "__main__":
     main()
