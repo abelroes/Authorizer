7ffdc387941274103b0e3b6429b907904d887f4f
Abel Rocha <abel.roes@gmail.com>
2021-10-03 20:06:07 -0300

    Change name for handle_transaction

--- src/controllers/handlers/TransactionExecuterHandler.py
+++ src/controllers/handlers/TransactionExecuterHandler.py
@@ -1,3 +1,5 @@
 
-def handle_execute_transaction(transaction_operation: dict) -> dict:
-    pass
+def handle_transaction(transaction_operation: dict) -> dict:
+    validation_result = transaction_operation
+    validation_result["violation"] = ["bla-bla-bla"]
+    return validation_result
